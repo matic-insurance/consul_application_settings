19da400c77e562b3ac9bdf71a346d7e12180c0cd
Volodymyr Mykhailyk <712680+volodymyr-mykhailyk@users.noreply.github.com>
2019-11-28 20:04:15 +0200

    Default config and clear after test

--- spec/support/helpers/configuration.rb
+++ spec/support/helpers/configuration.rb
@@ -1,6 +1,6 @@
 module Helpers
   module Configuration
-    def configure_settings(defaults_fixture, &block)
+    def configure_settings(defaults_fixture = 'simple', &block)
       ConsulApplicationSettings.configure do |config|
         config.defaults_path = defaults_fixture_path(defaults_fixture)
         yield(config) if block
@@ -10,9 +10,19 @@ module Helpers
     def defaults_fixture_path(defaults_fixture)
       File.expand_path("../fixtures/#{defaults_fixture}.yml", __dir__)
     end
+
+    def clear_gem_configs
+      ConsulApplicationSettings.config = ConsulApplicationSettings::Configuration.new
+    end
   end
 end
 
 RSpec.configure do |config|
   config.include(Helpers::Configuration)
+
+  config.around(:each) do |example|
+    configure_settings
+    example.run
+    clear_gem_configs
+  end
 end
