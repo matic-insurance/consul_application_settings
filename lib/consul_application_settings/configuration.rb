d01a836e6814ca5444b659e028e8560f6e75695f
Dan Polyuha <dan.p@matic.com>
2020-05-22 14:22:26 +0300

    "application_settings.yml" -> "app_settings.yml"

--- lib/consul_application_settings/configuration.rb
+++ lib/consul_application_settings/configuration.rb
@@ -1,8 +1,8 @@
 module ConsulApplicationSettings
   # All gem configuration settings
   class Configuration
-    DEFAULT_BASE_FILE_PATH = 'config/application_settings.yml'.freeze
-    DEFAULT_LOCAL_FILE_PATH = 'config/application_settings.local.yml'.freeze
+    DEFAULT_BASE_FILE_PATH = 'config/app_settings.yml'.freeze
+    DEFAULT_LOCAL_FILE_PATH = 'config/app_settings.local.yml'.freeze
     attr_accessor :base_file_path, :local_file_path, :disable_consul_connection_errors
 
     def initialize
