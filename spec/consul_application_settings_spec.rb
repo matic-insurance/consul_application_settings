456e88a1bddfffd12d52854aaf16d41dc44276d5
Volodymyr Mykhailyk <712680+volodymyr-mykhailyk@users.noreply.github.com>
2021-07-09 13:40:16 +0300

    Add performance tests to the spec

--- spec/consul_application_settings_spec.rb
+++ spec/consul_application_settings_spec.rb
@@ -40,6 +40,19 @@ RSpec.describe ConsulApplicationSettings do
     end
   end
 
+  describe 'performance', :default_settings_file do
+    it 'should be faster then 0.1ms for one read' do
+      settings = ConsulApplicationSettings.load
+      execution_time = Benchmark.measure { 1000.times { settings.get('application/name') } }
+      expect(execution_time.real).to be < 0.1
+    end
+
+    it 'should be faster then 20ms for load' do
+      execution_time = Benchmark.measure { 100.times { ConsulApplicationSettings.load } }
+      expect(execution_time.real).to be < 2
+    end
+  end
+
   describe '.configure' do
     let(:local_file_path) { 'path2' }
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'simplecov'
 require 'bundler/setup'
+require 'benchmark'
 require 'consul_application_settings'
 
 Dir[File.expand_path('support/**/*.rb', __dir__)].each { |f| require f }
