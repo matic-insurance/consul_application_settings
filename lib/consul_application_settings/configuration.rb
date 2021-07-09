90b31535bbb01e3c2b2c7a5a0edd96d81068ad1b
Volodymyr Mykhailyk <712680+volodymyr-mykhailyk@users.noreply.github.com>
2021-07-09 13:39:36 +0300

    Change default providers to preloaded consul

--- lib/consul_application_settings/configuration.rb
+++ lib/consul_application_settings/configuration.rb
@@ -4,7 +4,7 @@ module ConsulApplicationSettings
     DEFAULT_BASE_FILE_PATH = 'config/app_settings.yml'.freeze
     DEFAULT_LOCAL_FILE_PATH = 'config/app_settings.local.yml'.freeze
     DEFAULT_PROVIDERS = [
-      ConsulApplicationSettings::Providers::Consul,
+      ConsulApplicationSettings::Providers::ConsulPreloaded,
       ConsulApplicationSettings::Providers::LocalStorage
     ]
     attr_accessor :base_file_path, :local_file_path, :disable_consul_connection_errors, :settings_providers
